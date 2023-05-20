555756dcbaa009259780b256e1598c1b0e88563d
Mohd Rejoan <wearecrx@gmail.com>
2023-05-20 22:16:18 +0600

    photographer list added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// photographer list slider 
+// photographer list slider
+// $(document).ready(function () {
+//   $("#photographer-list").slick()
+// })
+
+
+$(document).ready(function () {
+  $("#photographer-list .slides-wrap").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    nextArrow: '.next-slide-btn',
+    prevArrow: '.prev-slide-btn'
+  });
+})
