3ec1b48e9aeff8410e90767e40930593d8d1282f
Mohd Rejoan <wearecrx@gmail.com>
2023-05-21 21:18:38 +0600

    slider dynamic width added

--- js/script.js
+++ js/script.js
@@ -4,9 +4,24 @@
 // })
 
 $(document).ready(function () {
+    // slider dynamic width
+    function sliderWidthHandler() {
+        // set slider dynamic width
+        $("#schedule-header .shadow").width(function (_, width) {
+            $("#schedule-header #photographer-list").width(width);
+        });
+      
+    }
+
+  sliderWidthHandler();
+  // set width on window resize 
+  $(window).on('resize', function () {
+    sliderWidthHandler();
+  })
+  
+    // slider creator
     const sliderNextArrow = "#schedule-header .next-btn";
     const sliderPrevArrow = "#schedule-header .prev-btn";
-
     const photographerListSlider = $(
         "#schedule-header #photographer-list .slides-wrap"
     ).slick({
